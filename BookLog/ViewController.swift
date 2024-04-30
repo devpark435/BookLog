bd8fe59c1aa5831c66e8a584fa382f421d539761
HyunRyeol Park <devpark435@gmail.com>
2024-05-01 00:14:23 +0900

    Create Project

---
+++ BookLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookLog
+//
+//  Created by 박현렬 on 5/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
