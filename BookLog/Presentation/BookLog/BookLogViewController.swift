e4092075332c25996f8908dfa2b2a33557bbe1c3
HyunRyeol Park <devpark435@gmail.com>
2024-05-01 02:09:02 +0900

    [Feat] #4 - UITabBarController Test 용 VC 제작

--- BookLog/Presentation/BookLog/BookLogViewController.swift
+++ BookLog/Presentation/BookLog/BookLogViewController.swift
@@ -5,4 +5,12 @@
 //  Created by 박현렬 on 5/1/24.
 //
 
-import Foundation
+import UIKit
+
+class BookLogViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
