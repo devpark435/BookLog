e4a910084795652f49236f485aa8d70e02afe6d6
HyunRyeol Park <devpark435@gmail.com>
2024-05-01 19:45:38 +0900

    [Feat] #6 - UISearchBar UI Update

--- BookLog/Presentation/Search/SearchViewController.swift
+++ BookLog/Presentation/Search/SearchViewController.swift
@@ -6,11 +6,50 @@
 //
 
 import UIKit
+import Then
+import SnapKit
+import Kingfisher
 
 class SearchViewController: UIViewController{
+    
+    let searchController = UISearchController().then {
+        $0.searchBar.placeholder = "책 제목, 저자, 출판사를 검색하세요"
+        $0.obscuresBackgroundDuringPresentation = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        changeStatusBarBgColor(bgColor: UIColor.white)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
+        view.backgroundColor = .gray
+        navigationController?.navigationBar.backgroundColor = .white
+        self.navigationItem.searchController = searchController
     }
+    
+    func changeStatusBarBgColor(bgColor: UIColor?) {
+        if #available(iOS 13.0, *) {
+            if #available(iOS 15.0, *) {
+                let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+                let window = windowScene?.windows.first
+                let statusBarManager = window?.windowScene?.statusBarManager
+                let statusBarView = UIView(frame: statusBarManager?.statusBarFrame ?? .zero)
+                statusBarView.backgroundColor = bgColor
+                window?.addSubview(statusBarView)
+            } else {
+                let window = UIApplication.shared.windows.first
+                let statusBarManager = window?.windowScene?.statusBarManager
+                let statusBarView = UIView(frame: statusBarManager?.statusBarFrame ?? .zero)
+                statusBarView.backgroundColor = bgColor
+                window?.addSubview(statusBarView)
+            }
+        } else {
+            let statusBarView = UIApplication.shared.value(forKey: "statusBar") as? UIView
+            statusBarView?.backgroundColor = bgColor
+        }
+    }
+    
 }
