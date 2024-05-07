6fac6fba5b2c4d5f2a695abcf3f55451df9b17f7
HyunRyeol Park <devpark435@gmail.com>
2024-05-07 22:06:08 +0900

    [Feat] #6 - 검색 Keyword기반 관련 검색어 추천 ViewController 제작

---
+++ BookLog/Presentation/Search/KeywordViewController.swift
@@ -0,0 +1,63 @@
+//
+//  KeywordViewController.swift
+//  BookLog
+//
+//  Created by 박현렬 on 5/7/24.
+//
+
+import UIKit
+
+class KeywordViewController: UIViewController{
+    
+    let keywordCellIdentifier = "KeywordCell"
+    
+    var keywordSearchItems: [String] = []
+    
+    let keywordTableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        
+        setupTableView()
+        setupLayout()
+    }
+    
+    private func setupTableView() {
+        keywordTableView.dataSource = self
+        keywordTableView.delegate = self
+        keywordTableView.register(UITableViewCell.self, forCellReuseIdentifier: keywordCellIdentifier)
+    }
+    
+    private func setupLayout() {
+        view.addSubview(keywordTableView)
+        keywordTableView.snp.makeConstraints{
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+}
+
+extension KeywordViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return keywordSearchItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: keywordCellIdentifier, for: indexPath)
+        cell.textLabel?.text = keywordSearchItems[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+}
+
+extension KeywordViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected")
+    }
+}
