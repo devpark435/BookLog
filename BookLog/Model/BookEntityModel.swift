2611b21f555af297a5492fb4c8f0f38db55bc21d
HyunRyeol Park <devpark435@gmail.com>
2024-05-08 22:59:40 +0900

    [Feat] #10 - CoreData 저장용 Model 생성

---
+++ BookLog/Model/BookEntityModel.swift
@@ -0,0 +1,17 @@
+//
+//  BookEntityModel.swift
+//  BookLog
+//
+//  Created by 박현렬 on 5/8/24.
+//
+
+import Foundation
+
+struct BookEntityModel {
+    let authors: String
+    let contents: String
+    let publisher: String
+    let thumbnail: String
+    let title: String
+    let review: String?
+}
