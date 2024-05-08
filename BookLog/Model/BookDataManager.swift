4932cd6af2e0e2e5cf0e5a3f81fa3de6ac8bfaab
HyunRyeol Park <devpark435@gmail.com>
2024-05-08 23:00:14 +0900

    [Feat] #10 - CoreData Read

--- BookLog/Model/BookDataManager.swift
+++ BookLog/Model/BookDataManager.swift
@@ -28,24 +28,53 @@ class BookDataManager{
     // MARK: - CRUD Operations
     
     // Create
-    func saveBook(book: Book) {
+    func saveBook(book: BookEntityModel) {
         let managedContext = persistentContainer.viewContext
         let bookEntity = NSEntityDescription.entity(forEntityName: "BookEntity", in: managedContext)!
         let bookObject = NSManagedObject(entity: bookEntity, insertInto: managedContext)
         
-        bookObject.setValue(book.title, forKey: "title")
-        bookObject.setValue(book.authors.joined(separator: ", "), forKey: "authors")
+        bookObject.setValue(book.authors, forKey: "authors")
+        bookObject.setValue(book.contents, forKey: "contents")
         bookObject.setValue(book.publisher, forKey: "publisher")
         bookObject.setValue(book.thumbnail, forKey: "thumbnail")
-        bookObject.setValue(book.contents, forKey: "contents")
+        bookObject.setValue(book.title, forKey: "title")
         bookObject.setValue(book.review, forKey: "review")
-        // Set other book properties
         
         do {
             try managedContext.save()
-            print("Book saved.")
+            print("Book saved successfully")
         } catch let error as NSError {
             print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    // Read
+    func fetchBooks() -> [BookEntityModel] {
+        let managedContext = persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "BookEntity")
+        
+        do {
+            let bookObjects = try managedContext.fetch(fetchRequest)
+            var books: [BookEntityModel] = []
+            
+            for bookObject in bookObjects {
+                let authors = bookObject.value(forKey: "authors") as? String ?? ""
+                let contents = bookObject.value(forKey: "contents") as? String ?? ""
+                let publisher = bookObject.value(forKey: "publisher") as? String ?? ""
+                let thumbnail = bookObject.value(forKey: "thumbnail") as? String ?? ""
+                let title = bookObject.value(forKey: "title") as? String ?? ""
+                let review = bookObject.value(forKey: "review") as? String
+                
+                let book = BookEntityModel(authors: authors, contents: contents, publisher: publisher, thumbnail: thumbnail, title: title, review: review)
+                books.append(book)
+            }
+            
+            print(books)
+            
+            return books
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+            return []
         }
     }
 }
